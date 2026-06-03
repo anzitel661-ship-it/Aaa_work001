09d4c974d5566db4b7c549c54a564b32e4d41978
Aaron <anzitel661@gmail.com>
2026-06-03 15:45:21 +0800

    change the ex01 program and README

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
+    char input[256];
+
     printf("Hello, World!\n");
+    printf("Please enter something: ");
+    fgets(input, sizeof(input), stdin);
+    printf("You entered: %s", input);
+
     return 0;
 }
